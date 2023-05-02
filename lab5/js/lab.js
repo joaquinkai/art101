3424eba08a6e7386df6bda201f9b23e353067bb7
joaquinkai <joaquinsma@gmail.com>
2023-05-02 10:29:33 -0700

    Completed Lab 5 and Changed site.css slightly.

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,39 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab5.js - Data Types and Variables
+// Author: Joaquin Jorgensen <jkjorgen@ucsc.edu>
+// Date: 2 May 2023
+// License: Public Domain
 
-// Constants
+// Define Variables
+var make = "Honda";
+var model = "CRV";
+var color = "Gray";
+var year = 2010;
+var ownIt = true;
 
-// Functions
+// Calculate
+var age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Car Owned: " + (ownIt ? "Yes" : "No") + "<br>");
+document.writeln("Age: " + age + "<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+// // Functions
+
+// // this is an example function and this comment tells what it doees and what parameters are passed to it.
+// function myFunction(param1, param2) {
+//   // some code here
+//   // return results;
+// }
+
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
+
+// // let's get this party started
+// main();
